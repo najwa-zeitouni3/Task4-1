588ad7e198866a06ce907b60ad7138c95a615b31
najwa zeitouni <najozeitouni77@gmail.com>
2023-09-06 19:36:35 +0300

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,39 @@
+var array=['1','2','3','4','5','6','7','8','9','10']
+for(i=0;i<10;i++){
+   if(i%2==0){
+       console.log(i);
+   }
+}
+console.log(i);
+document.write('0')
+document.write('2')
+document.write('4')
+document.write('6')
+document.write('8')
+document.write('10')
+
+
+var array=['1','2','3','4','5','6','7','8','9','10']
+var sum=0
+for(var index=0;index<array.length;index++){
+    if(number[index]<5){
+        sum+=number[index];
+        console.log(sum);
+
+    }
+}
+
+
+document.write('10')
+
+var object={
+    name:'najwa',
+     age:'25',
+   number:'0945131508',
+     email:'najozeitouni77@gmail.com',
+    }
+    console.log(object);
+    console.log(object.name);
+    console.log(object.age);
+    console.log(object.number);
+    console.log(object.email);
